3dd6937968df92bf0d5fb26c7425ffc0576a4835
gglaive <gglaive@gmail.com>
2019-02-05 20:19:45 +0100

    --varable, aucune idée pk ct là

---
+++ Item.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Item {
+
+	private String name;
+	private int number;
+	public int initial_number;
+
+	public Item(String name, int number) {
+		this.name = name;
+		this.number = number;
+		initial_number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	static public Item findByName(String s, ArrayList<Item> L) {
+        int n=0;
+        while (n < L.size()) {
+            Item i = L.get(n);
+            if (s.equals(i.getName())) 
+                return i;
+            n++;
+        }
+        return null;    // not found above
+    }
+	
+}
